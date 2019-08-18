fdc6dabbf75c9902ee8caad86bbb89aa51c9a5ad
FuryIsHere <42156367+FuryIsHere@users.noreply.github.com>
2019-08-18 23:37:07 +0100

    123

--- js/RandomBackground.js
+++ js/RandomBackground.js
@@ -23,9 +23,9 @@ if (screenWidth <= 768){
         const num = Math.ceil(Math.random() * backgroundImages.length - 1);
         phoneBackground.style.background = `url('./images/${backgroundImages[num]}.png')`;
         phoneBackground.style.backgroundRepeat = "no-repeat";
-        phoneBackground.style.backgroundSize = "cover";
-        phoneBackground.style.display = "block";
-        phoneBackground.style.margin = "0 auto";
+        phoneBackground.style.backgroundAttachment = "fixed";
+        phoneBackground.style.backgroundPosition = "50% 0%";
+        phoneBackground.style.overflow = "0";
     })
 } else {
     window.addEventListener('DOMContentLoaded', () => {
